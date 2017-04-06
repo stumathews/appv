ae31e9c38a155386edcc57737c2f9de6f4bd9fa8
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-04-06 16:54:50 +0100

    added TestClassLibrary

---
+++ XDAppVOfflinePlugin/TestClassLibrary/ServiceBrokerProtocol.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace TestClassLibrary
+{
+
+    /// <summary>
+    /// Request object used to communicate from ther plugin to the service
+    /// </summary>
+    [Serializable]
+    public class Request
+    {
+        /// <summary>
+        /// The requested task
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "This is to be a flat object with no function as its a protocol object")]
+        public RequestTask RequestTask;
+        /// <summary>
+        /// The message body
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "This is to be a flat object with no function as its a protocol object")]
+        public string MessageBody;
+        public Request() {}
+    }
+
+    /// <summary>
+    /// Response object used to communicate response information from the serivce to the plugin
+    /// </summary>
+    [Serializable]
+    public class Response
+    {
+        /// <summary>
+        /// The response code
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "This is to be a flat object with no function as its a protocol object")]
+        public ResponseCode ResponseCode;
+        /// <summary>
+        /// The message body
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1051:DoNotDeclareVisibleInstanceFields", Justification = "This is to be a flat object with no function as its a protocol object")]
+        public string MessageBody;
+        public Response() { }
+    }
+
+    public enum ResponseCode
+    {
+        /// <summary>
+        /// The response code used to indicate that function was a void function, that has no response information
+        /// </summary>
+        None,
+        /// <summary>
+        /// The response code used to indicate that their was a error while executing the appv service code(admin functions)
+        /// </summary>
+        Error,
+    }
+
+    /// <summary>
+    /// This enum edientifies the task asked to be carried out by the service
+    /// </summary>
+    public enum RequestTask
+    {
+        AddClientPackage,
+        AddPublishingServer,
+        UpdateIsolationGroups,
+        RemoveAllPublishingServers,
+        ConfigureSharedContentStoreMode,
+        CleanupCache,
+        StopNamedPipeServer
+    }
+
+    /// <summary>
+    /// Helper utilities
+    /// </summary>
+    public static class ServiceBrokerProtocolHelper
+    {
+        /// <summary>
+        /// Serializes the specified protocol object.
+        /// </summary>
+        /// <typeparam name="T">Object type you'd like to serialize</typeparam>
+        /// <param name="protocolObject">The protocol object.</param>
+        /// <returns>xml string representing the object</returns>
+        public static string Serialize<T>(T protocolObject)
+        {
+            var xmlSerializer = new XmlSerializer(typeof (T));
+            using (var sw = new StringWriter(CultureInfo.CurrentCulture))
+            {
+                xmlSerializer.Serialize(sw, protocolObject);
+                return sw.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Deserializes the specified raw request.
+        /// </summary>
+        /// <typeparam name="T">Object type you'd like to deserialize</typeparam>
+        /// <param name="rawRequest">The raw request.</param>
+        /// <returns>the object, deserialized</returns>
+        public static T Deserialize<T>(string rawRequest)
+        {
+            var xmlSerializer = new XmlSerializer(typeof (T));
+            using (var textReader = new XmlTextReader(new StringReader(rawRequest)))
+            {
+                return (T) xmlSerializer.Deserialize(textReader);
+            }
+        }
+
+        public static Response SendRequest(Request request, Stream pipeClient, Action<string,string> loggingFunction) 
+        {
+            loggingFunction(MethodBase.GetCurrentMethod().Name,"Sending request to service...");
+            var streamString = new StreamString(pipeClient);
+            
+            loggingFunction(MethodBase.GetCurrentMethod().Name,"Serializing request...");
+            var requestXml = ServiceBrokerProtocolHelper.Serialize(request);
+
+            loggingFunction(MethodBase.GetCurrentMethod().Name,"Transmitting request...");
+
+            // Send request
+            streamString.WriteString(requestXml);
+            loggingFunction(MethodBase.GetCurrentMethod().Name,"Request sent to service.");
+
+            loggingFunction(MethodBase.GetCurrentMethod().Name,"Receiving response from service...");
+            
+            //Recieve the response
+            var response =  ServiceBrokerProtocolHelper.Deserialize<Response>(streamString.ReadString());
+            loggingFunction(MethodBase.GetCurrentMethod().Name,string.Format(CultureInfo.CurrentCulture,"Response received from service: Code={0}, MessageBody={1}", response.ResponseCode, response.MessageBody));
+            return response;
+        }
+    }
+}
---
+++ XDAppVOfflinePlugin/TestClassLibrary/StreamString.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestClassLibrary
+{
+    /// <summary>
+    /// Reads and writes strings to an IOStream using a custom protocol to serialize the packets.
+    /// <remarks>Both the server and client shoud use this class to receive and send respectively to manage internal custom protocol</remarks>
+    /// </summary>
+    public class StreamString
+    {
+        private readonly Stream _ioStream;
+        private readonly UnicodeEncoding _streamEncoding;
+
+        public StreamString(Stream ioStream)
+        {
+            _ioStream = ioStream;
+            _streamEncoding = new UnicodeEncoding();
+        }
+
+        /// <summary>
+        /// Reads the string.
+        /// </summary>
+        /// <returns></returns>
+        public string ReadString()
+        {
+            // How many bytes should I(the server) expect for the message length?
+            var lenBytes = _ioStream.ReadByte();
+            byte[] messageLength = new byte[lenBytes];
+            _ioStream.Read(messageLength, 0, lenBytes);
+
+            // Interpret the expected bytes as the length of the incomming message
+            var actualLength = BitConverter.ToInt16(messageLength,0);
+
+            // read the rest of actually string message as determined by the actualLength above
+            byte[] inBuffer = new byte[actualLength];
+            _ioStream.Read(inBuffer, 0, actualLength);
+
+            return _streamEncoding.GetString(inBuffer);
+        }
+
+        /// <summary>
+        /// Writes the string.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public int WriteString(string value)
+        {
+            byte[] outBuffer = _streamEncoding.GetBytes(value);
+
+            // Determine how many bytes it will take to store a number as large as outBuffer.Length
+            byte[] lengthBytes = BitConverter.GetBytes(outBuffer.Length);
+            
+            // Tell the server to expect lengthBytes that it should use to interpret as the length of bytes it must use to interpret the size of the message size
+            _ioStream.WriteByte((byte)lengthBytes.Length);
+            
+            // Now send the actual bytes
+            _ioStream.Write(lengthBytes, 0, lengthBytes.Length);
+            
+            // Now send the actual message which is of a length that can be stored in lengthBytes of bytes.
+            _ioStream.Write(outBuffer, 0, outBuffer.Length);
+            _ioStream.Flush();
+
+            return outBuffer.Length + 2;
+        }
+    }
+}
