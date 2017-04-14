dc8fc3a50b9aedd10edcee0633830ba91d863b85
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-04-14 16:46:18 +0100

    changes to .net library

---
+++ XDAppVOfflinePlugin/TestClassLibrary/TestClassInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestClassLibrary
+{
+    public interface TestClassInterface
+    {
+        int AddAsd(string asd, ref string responseText);
+        int return5();
+        string returnHello();
+        string echo(string msg);
+    }
+}
---
+++ XDAppVOfflinePlugin/TestClassLibrary/TestClassInterfaceFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace TestClassLibrary
+{
+    public class TestClassInterfaceFunctions : TestClassInterface
+    {
+        public int AddAsd(string asd, ref string responseText)
+        {
+            if (responseText == null) throw new ArgumentNullException("responseText");
+            var client = new AppVService((s, s1) => { });
+            var response = client.SendMessage(new Request() {MessageBody = asd, RequestTask = RequestTask.AddClientPackage});
+            responseText = response.MessageBody;
+            return (int)response.ResponseCode;
+        }
+
+    public string echo( string msg )
+    {
+      return msg;
+    }
+
+    public int return5()
+    {
+      return 5;
+    }
+
+    public string returnHello()
+    {
+      return "Hello";
+    }
+  }
+}
