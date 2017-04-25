eca668772e3c8e7d0068ed379c71db4c033d47f2
Stuart Mathews <Stuart.Mathews@citrix.com>
2017-04-25 15:15:53 +0100

    whatever changes i need em

--- XDAppVOfflinePlugin/TestClassLibrary/TestClassInterfaceFunctions.cs
+++ XDAppVOfflinePlugin/TestClassLibrary/TestClassInterfaceFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 
 namespace TestClassLibrary
@@ -7,11 +8,16 @@ namespace TestClassLibrary
     {
         public int AddAsd(string asd)
         {
-            
-            var client = new AppVService((s, s1) => { });
-            var response = client.SendMessage(new Request() {MessageBody = "test", RequestTask = RequestTask.AddClientPackage});
-            //SresponseText = response.MessageBody;
-            return (int)6;
+            try
+            {
+              var client = new AppVService((s, s1) => { });
+              var response = client.SendMessage(new Request() {MessageBody = "test", RequestTask = RequestTask.AddClientPackage});
+              return (int)response.MessageBody.Length;
+            }
+            catch(Exception e)
+            {
+              return -1;
+            }      
         }
 
     public string echo( string msg )
@@ -26,7 +32,21 @@ namespace TestClassLibrary
 
     public string returnHello()
     {
-      return "Hello";
+      string ret = string.Empty;
+      try
+      {
+         var client = new AppVService((s, s1) => {} );
+         var response = client.SendMessage(new Request() {MessageBody = @"462dfa90-9e09-429e-a6f3-00cb3e9bf90b;\\londondc.london.local\appvshare\EPM 3.appv;4148dc52-d7d5-47c3-a570-396aa63fa9fe;d2022911-4251-4c86-b8cd-e2bb092443fd;EPM Opsætningsmodul", RequestTask = RequestTask.AddClientPackage});
+         ret = string.Format("{0}{1}", response.ResponseCode, response.MessageBody);
+      }
+      catch(Exception e)
+      {
+        const int max_length = 200;
+        string message = e.Message + e.StackTrace;
+        
+        return message.Length <= max_length? message : message.Substring(0,max_length);
+      }
+      return string.IsNullOrEmpty(ret) ? "Hello" : ret;
     }
   }
 }
